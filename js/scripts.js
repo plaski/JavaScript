d09791b044793141d52ad63ba8b54d433b7ecba9
plaski <maciejploskonka@gmail.com>
2017-02-22 20:35:30 +0100

    dodany else przy rownym zero i zmienione dzialanie na prawidlowe

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var a = prompt('Podaj pierwszą liczbę');
 	b = prompt('Podaj drugą liczbę');
-	value = (a*a) + (2*a*b) + (b*b);
+	value = (a*a) + (2*a*b) - (b*b);
 
 console.log(value);
 
@@ -8,4 +8,6 @@ if (value > 0) {
 	alert('Wynik to: ' +value+ ' i jest większy od zera.');
 } else if (value < 0) {
 	alert('Wynik to: ' +value+ ' i jest mniejszy od zera.');
-};
+} else {
+	alert('Wynik to: ' +value+ ' i jal widać jest równy zeru.');
+}
