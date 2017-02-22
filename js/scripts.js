ecd38b4d7986ca89b8b6e49546e5389540938ab9
plaski <maciejploskonka@gmail.com>
2017-02-22 19:36:46 +0100

    dodane prompty o liczby

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var a = 3;
-	h = 4;
+var a = prompt('Podaj podstawę trójkąta.');
+	h = prompt('Podaj wysokość trójkąta.');
 	triangleArea = a*h/2;
 
 console.log('Pole trójkąta o podstawie a: ' +a+ ' i wysokości h: ' +h+ ' wynosi: ' +triangleArea);
