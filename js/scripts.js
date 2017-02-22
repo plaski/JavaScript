8a35cdfe7681e2af53d05267b8a13938e816f4af
plaski <maciejploskonka@gmail.com>
2017-02-22 21:16:55 +0100

    pola trojkatow trzech

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var triangle1Area = getTriangleArea(6,8);
+var triangle2Area = getTriangleArea(8,13);
+var triangle3Area = getTriangleArea(21,34);
+
+function getTriangleArea(a, h) {
+	if (a < 0 || h < 0) {
+		alert('Nieprawidłowe dane');
+	} else {
+		return value = a*h/2;
+	}
+}
+
+console.log(getTriangleArea(10,6))
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
