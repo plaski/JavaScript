b02fcbd736aea216c0170d5b061142150bf81971
plaski <maciejploskonka@gmail.com>
2017-02-23 21:01:20 +0100

    choinka

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+function rysujChoinke(wysokoscChoinki) {
+	for (var i = 0; i < wysokoscChoinki; i++) {
+		var star = '';
+		
+		for (var i = 0; i < wysokoscChoinki*2-1; i++) {
+			star += '*';
+		}
+		console.log(star);
+	}
+}
+var wysokoscChoinki = prompt('Podaj liczbę poziomów choinki.');
+rysujChoinke(wysokoscChoinki);
