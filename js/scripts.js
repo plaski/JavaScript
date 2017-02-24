88fdefaa06c90427ad06c585d54c2439a2a67427
plaski <maciejploskonka@gmail.com>
2017-02-24 23:02:32 +0100

    adding and removing items

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+var rem = document.getElementById('remElem');
+var number = document.getElementsByTagName('li');
+add.addEventListener('click', function(){
+	list.innerHTML += '<li>item ' + number.length + '</li>'
+});
+rem.addEventListener('click', function(){
+	list.removeChild(list.childNodes[number.length+1])
+});
