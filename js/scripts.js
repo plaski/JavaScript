b6e35c233d97a4d38a353f96201bfdb4c2b0ba52
plaski <maciejploskonka@gmail.com>
2017-02-24 15:34:05 +0100

    symmetrical tree

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 function rysujChoinke(wysokoscChoinki) {
 	for (var i = 1; i <= wysokoscChoinki; i++) {
 		var star = '';
-
+		for (var m = wysokoscChoinki; m > i; m--) {
+			star += ' ';
+		}
 		for (var n = 0; n < i*2-1; n++) {
+
 			star += '*';
 		}
 		console.log(star);
